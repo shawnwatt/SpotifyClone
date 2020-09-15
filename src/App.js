9d8e1ace9162e105e62f6254ed7d1ad6a85f6d2d
shawnwatt <mrwatt68@gmail.com>
2020-09-15 10:30:55 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* login */}
+      {/* header */}
+      {/* body */}
+      {/* footer */}
+     
+    </div>
+  );
+}
+
+export default App;
